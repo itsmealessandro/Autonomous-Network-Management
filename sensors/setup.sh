a645a62b435638bdea211666bd3d1866313add0e
itsmealessandro <129402291+itsmealessandro@users.noreply.github.com>
2025-05-27 18:43:59 +0200

    save before new activator with debug 3

--- sensors/setup.sh
+++ sensors/setup.sh
@@ -4,15 +4,16 @@ echo "hello from setup.sh"
 
 cd ./app/4_autoGenericBundleInstaller/osgiJars/
 
-echo "i'm in: $(pwd)"
-echo "----------------------------"
-
-ls -la
-echo "----------------------------"
+#echo "i'm in: $(pwd)"
+#echo "----------------------------"
+#
+#ls -la
+#echo "----------------------------"
 
 chmod +x org.eclipse.equinox.console_1.4.800.v20240513-1104.jar
 
-ls -la
+#ls -la
 echo "----------------------------"
+echo "lunching the console"
 
 java -jar org.eclipse.osgi_3.21.0.v20240717-2103.jar -console
